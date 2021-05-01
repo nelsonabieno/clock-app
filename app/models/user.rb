342cc15bfe3182e93a4f2a64cb8398dccb334d30
Rotimi Nelson <nelsonabieno@gmail.com>
2021-05-01 17:05:52 +0100

    add model validations and configure bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :clock_events
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :password, presence: true
+  validates :email, presence: true, unique: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_153134) do
+ActiveRecord::Schema.define(version: 2021_05_01_155813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_153134) do
     t.string "firstname"
     t.string "lastname"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
