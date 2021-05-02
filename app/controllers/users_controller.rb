c195247bdcf53f27193280569fa359804a6fff33
Rotimi Nelson <nelsonabieno@gmail.com>
2021-05-02 22:55:29 +0100

    implement user login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,41 @@
 class UsersController < ApplicationController
+  before_action :new_user, only: [:sign_up, :login]
+
+  def sign_up
+  end
+
+  def login
+  end
+
+  def authenticate
+    @user = User.find_by(email: params[:user][:email])
+
+    if @user.present? && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: "Successfully Logged In"
+    else
+      flash[:alert]= "Invalid username or password"
+      render :login
+    end
+  end
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      flash[:message] = "User successfully created"
-      redirect_to login_path
+      redirect_to login_path, notice: "User successfully created"
     else
-      @errors = @user.errors.full_messages
+      render :sign_up
     end
   end
 
   private
 
+  def new_user
+    @user = User.new
+  end
+
   def user_params
-    params.require(:user).permit(:firstname, :lastname, :email, :password)
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/" => "home#index"
+  root "home#index"
   get "/login" => "users#login"
+  post "/login" => "users#authenticate"
   get "/sign_up" => "users#sign_up"
   post "/sign_up" => "users#create"
   post "/clock_in" => "clockevents#clock_in"
